c5c5efebe0f7ae574f05aae30f845293462e30d1
bcoutinov <127880230+bcoutinov@users.noreply.github.com>
2023-05-04 10:58:13 -0700

    Create client.js

---
+++ client.js
@@ -0,0 +1,33 @@
+/*	Author: 	Carlton Louie, Brenda Coutino
+	Class:  	CSC 337
+	File:		client.js
+	Assignment:	Final Project
+	This file is the JS for the client-side of the service desk
+	*/
+var t = document.getElementById("title").value;
+var d = document.getElementById("desc").value;
+var p = document.getElementById("priority").value;
+var s = document.getElementById("stat").value;
+var data = { title: t, 
+    desc: d,
+    price: p,
+    status: s,
+    image: i
+};
+
+var url = "" //  TODO: add url
+
+//  Adds a ticket
+function addTicket(){ 
+    //  TODO
+}
+
+//  Logs user into website checking if account is already existing
+function login(){ 
+    //  TODO
+}
+
+//  Creates an account
+function create(){
+    //  TODO
+}

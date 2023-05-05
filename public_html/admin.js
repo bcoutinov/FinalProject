709e17317fd19afca5a1820d5bbdcd285cb937b7
bcoutinov <127880230+bcoutinov@users.noreply.github.com>
2023-05-04 22:28:53 -0700

    Create admin.js

---
+++ public_html/admin.js
@@ -0,0 +1,6 @@
+/*	Author: 	Carlton Louie, Brenda Coutino, Kianny Calvo
+	Class:  	CSC 337
+	File:		admin.js
+	Assignment:	Final Project
+	This file is the JS for the admin-side of the service desk
+	*/

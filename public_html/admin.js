fcb603bbbaff7f85e2dfbe67c01197804e5ab294
bcoutinov <127880230+bcoutinov@users.noreply.github.com>
2023-05-05 11:32:40 -0700

    Update admin.js

--- public_html/admin.js
+++ public_html/admin.js
@@ -4,3 +4,39 @@
 	Assignment:	Final Project
 	This file is the JS for the admin-side of the service desk
 	*/
+
+
+// Takes tickets and displays them on the webpage
+function showtickets(){
+	
+}
+
+
+// Takes items and displays them on the webpage
+function showListings(responseText){
+    let listings = JSON.parse(responseText);
+    let listingStrings = [];
+
+    for (let i in listings){
+        console.log(listings[i].title);
+        let listingString = '<div class="listing">';
+        let title = '<h3 class="listingTitle">' + listings[i].title + '</h3>';
+        let ps = '<h4 class="ps"> Price: $' + listings[i].price + '\t Status: ' + listings[i].stat + '</h4>';
+        let desc = '<p>' + listings[i].description + '</p>';
+
+        if (listings[i].stat != 'sold'){
+            let pFunc = "purchase('"+listings[i]._id+"');"
+            var pButton =  '<button type="submit" onclick="'+pFunc+'">Buy</button>';
+        }
+        else{
+            var pButton = '<p>SOLD</p>';
+        }
+
+        listingString = listingString + title + ps + desc + pButton + "</div>";
+        listingStrings.push(listingString);
+    }
+
+    let htmlText =  listingStrings.join("");
+
+    itemDisp.innerHTML = htmlText;
+}

0d52486126b938f566e40aae252c733b687b8b89
bcoutinov <127880230+bcoutinov@users.noreply.github.com>
2023-05-04 22:16:49 -0700

    Update client.js

--- client.js
+++ client.js
@@ -1,63 +1,16 @@
-/*	Author: 	Carlton Louie, Brenda Coutino
-	Class:  	CSC 337
-	File:		client.js
-	Assignment:	Final Project
-	This file is the JS for the client-side of the service desk
-	*/
-var t = document.getElementById("title").value;
-var d = document.getElementById("desc").value;
-var p = document.getElementById("priority").value;
-var s = document.getElementById("stat").value;
-var y = document.getElementById("type").value;
-var a = document.getElementById("date").value;
+/*  Author:     Carlton Louie, Brenda Coutino, Kianny Calvo
+    Class:      CSC 337
+    File:       client.js
+    Assignment: Final Project
+    This file is the JS for the login page
+    */
 
-var data = { title: t, 
-    desc: d,
-    price: p,
-    status: s,
-    image: i
-};
-
-var url = "" //  TODO: add url
-
-//  Adds a ticket
-function addTicket(){ 
-    let u = iUsr.value;
-    let t = title.value;
-    let d = desc.value;
-    let p = price.value;
-    let i = image.value;
-    let s = itemStatus.value;
-    
-    let requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            title: t,
-            desc: d,
-            price: p,
-            image: i,
-            status: s
-         })
-    };
-
-    fetch(url+'/add/item/'+u, requestOptions)
-        .then(response => {
-            if(response.redirected == true){
-                window.location.href = response.url;
-                console.log(response);
-            }
-        })
-        .catch((error) => {
-            console.log(error)
-        });
-}
 
 var loginU = document.getElementById("loginU");
 var loginP = document.getElementById("loginP");
 
-//  Logs user into website checking if account is already existing
-function login(){ 
+// Logs user into website
+function login(){
     let u = loginU.value;
     let p = loginP.value;
     validUsrName = loginU.value;
@@ -68,7 +21,6 @@ function login(){
         body: JSON.stringify({
             username : u,
             password: p,
-            //credentials: "same-origin"
          }),
         redirect: "follow"
     };
@@ -77,7 +29,6 @@ function login(){
         .then((response) => {
             if(response.redirected == true){
                 window.location.href = response.url;
-                //window.location.href = response.headers.get("Location");
             }
             else{
                 response.text()
@@ -94,8 +45,8 @@ function login(){
 var regU = document.getElementById("regU");
 var regP = document.getElementById("regP");
 
-//  Creates an account
-function create(){
+// Registers a user on the website
+function register(){
     let u = regU.value;
     let p = regP.value;
 
@@ -113,35 +64,4 @@ function create(){
         .then((text) => {
             alert(text);
         });
-}
-
-//  Adds a ticket
-function viewTicket(){ 
-    //  TODO
-}
-
-//  Adds a ticket
-function getUsrTicket(){ 
-
-    var httpRequest = new XMLHttpRequest();
-    if (!httpRequest) {
-        alert('Error');
-        return false;
-    }
-    httpRequest.onreadystatechange = () => {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if(httpRequest.responseURL == url + '/'){
-                window.location.href = httpRequest.responseURL;
-                //window.location.href = response.headers.get("Location");
-                console.log(response);
-            }
-            else if (httpRequest.status === 200) {
-                showListings(httpRequest.responseText);
-            } 
-            else { alert('Error fetching Items'); }
-        }
-    }
-
-    httpRequest.open('GET', url + "/get/listings/");
-    httpRequest.send();
 }

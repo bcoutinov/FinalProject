ba948fe44359ff555acae187a7f6eff69fbb8bc6
bcoutinov <127880230+bcoutinov@users.noreply.github.com>
2023-05-04 22:19:29 -0700

    Create home.js

---
+++ home.js
@@ -0,0 +1,6 @@
+/*  Author:     Carlton Louie, Brenda Coutino, Kianny Calvo
+    Class:      CSC 337
+    File:       client.js
+    Assignment: Final Project
+    This file is the JS for the client home of the service desk
+    */

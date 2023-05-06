934c33700b38cf6505dffd24f75612f88c64a839
k-arizona <127901292+k-arizona@users.noreply.github.com>
2023-05-05 20:30:10 -0700

    Update home.js

--- public_html/home.js
+++ public_html/home.js
@@ -3,7 +3,7 @@
     File:       home.js
     Assignment: Final Project
     This file is the JS for the client home of the service desk
-    */
+*/
 
 const url = "http://127.0.0.1"
 
@@ -34,7 +34,7 @@ function createTicket() {
     let p = fetch(url, {
       method: 'POST',
       body: JSON.stringify(data),
-      headers: {"Content-Type": "application/json"} // Adjust language with server
+      headers: {"Content-Type": "application/json"}
     });
     
     // Calls viewTicket()
@@ -50,72 +50,73 @@ function createTicket() {
 
 // Takes tickets and displays them on the webpage
 function getTickets(){
-  var httpRequest = new XMLHttpRequest();
-  if (!httpRequest) {
-      alert('Error');
-      return false;
+    var httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        alert('Error');
+        return false;
   }
-  httpRequest.onreadystatechange = () => {
-      if (httpRequest.readyState === XMLHttpRequest.DONE) {
-          if (httpRequest.status === 200) {
-              showTickets(httpRequest.responseText);
-          } 
-          else { alert('Error fetching Items'); }
-      }
-  }
+    httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                showTickets(httpRequest.responseText);
+            } 
+            else { alert('Error fetching Items'); }
+        }
+    }
 
-  httpRequest.open('GET', url + "/get/userTickets");
-  httpRequest.send();
+    httpRequest.open('GET', url + "/get/userTickets");
+    httpRequest.send();
 }
+
 
 var taskBody = document.getElementById("client-ticket-table");
 
 // Takes items and displays them on the webpage
 function showTickets(responseText){
-  let tasks = JSON.parse(responseText);
-  let taskStrings = [];
+    let tasks = JSON.parse(responseText);
+    let taskStrings = [];
   
-  // Gets the information on each ticket and pushes it as string
-  for (let i in tasks){
-      let tFunc = "openTicket('"+tasks[i]._id+"');";
-      let taskString = '<tr>';
-      let taskId = '<td onclick='+tFunc+'>' + tasks[i]._id.slice(19, 24) + '</td>';
-      let time = '<td>' + tasks[i].date + '</td>';
-      let priority = '<td>' + tasks[i].priority + '</td>';
-      let state = '<td>' + tasks[i].status + '</td>';
-      let title = '<td>' + tasks[i].title + '</td>';
-      taskString = taskString + taskId + title + time + priority + state + "</tr>";
-      taskStrings.push(taskString);
-  }
+    // Gets the information on each ticket and pushes it as string
+    for (let i in tasks){
+        let tFunc = "openTicket('"+tasks[i]._id+"');";
+        let taskString = '<tr>';
+        let taskId = '<td onclick='+tFunc+'>' + tasks[i]._id.slice(19, 24) + '</td>';
+        let time = '<td>' + tasks[i].date + '</td>';
+        let priority = '<td>' + tasks[i].priority + '</td>';
+        let state = '<td>' + tasks[i].status + '</td>';
+        let title = '<td>' + tasks[i].title + '</td>';
+        taskString = taskString + taskId + title + time + priority + state + "</tr>";
+        taskStrings.push(taskString);
+    }
 
-  let htmlText =  taskStrings.join("");
+    let htmlText =  taskStrings.join("");
 
-  taskBody.innerHTML = htmlText;
+    taskBody.innerHTML = htmlText;
 }
 
 // Opens the ticket view page
 function openTicket(id){
-  let requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-          ticketId : id,
-       }),
-  };
+    let requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            ticketId : id,
+        }),
+    };
   
-  // Fetch Request
-  fetch(url + '/post/ticketView', requestOptions)
-      .then((response) => {
-          if (response.status == 200){
-              window.location.href = '/ticketView';
-          }
-          else{
+    // Fetch Request
+    fetch(url + '/post/ticketView', requestOptions)
+        .then((response) => {
+            if (response.status == 200){
+                window.location.href = '/ticketView';
+            }
+            else {
               window.location.href = '/home';
-          }
-      })
-      .catch((error) => {
-          console.log(error);
-      });
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 getTickets()

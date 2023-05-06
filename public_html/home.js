0214514a4689dc7c2eebac0e4505dc61f616facb
bcoutinov <127880230+bcoutinov@users.noreply.github.com>
2023-05-05 19:56:08 -0700

    Update home.js

--- public_html/home.js
+++ public_html/home.js
@@ -1,6 +1,5 @@
 /*  Author:     Carlton Louie, Brenda Coutino, Kianny Calvo
     Class:      CSC 337
-    File:       client.js
     File:       home.js
     Assignment: Final Project
     This file is the JS for the client home of the service desk
